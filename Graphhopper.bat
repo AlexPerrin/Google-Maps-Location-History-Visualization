8a1664652231db942d44e32b4126f762a4354deb
unknown <16arp5@queensu.ca>
2020-03-17 23:05:33 -0400

    graphhopper

---
+++ Graphhopper.bat
@@ -0,0 +1,1 @@
+java -Xmx2g -Xms2g -jar graphhopper-web-0.9.0-with-dep.jar jetty.resourcebase=webapp config=config-example.properties datareader.file=ontario-latest.osm.pbf
--- pointconversion.py
+++ pointconversion.py
@@ -1,24 +1,26 @@
 import pandas as pd
 
+#Read Google data CSV into a pandas object
 coordinates = pd.read_csv('locations.csv', parse_dates=['Time'])
 coordinates['Date'] = coordinates['Time'].dt.date
 coordinates['Time'] = coordinates['Time'].dt.time
 coordinates = coordinates.reindex(columns = ['Latitude', 'Longitude', 'Date', 'Time'])
 
+#Conditions for removing points
+#detete points that have not moved atleast greater than 250m form the previous point (still plenty of points to used for routing)
+#but do not delete points at the same location if they are on different days, need these points as start and end point
+#delete points outside of the boundary of Kingston 
 sameDay = coordinates['Date'] == coordinates['Date'].shift()
 latitudeThresh = (abs(coordinates['Latitude'] - coordinates['Latitude'].shift()) < 0.0025) & sameDay
 longitudeThresh = (abs(coordinates['Longitude'] - coordinates['Longitude'].shift()) < 0.0025) & sameDay
 latitudeBoundry = (44.202942 < coordinates['Latitude']) & (coordinates['Latitude'] < 44.294006)
 longitudeBoundry = (-76.622925 < coordinates['Longitude']) & (coordinates['Longitude'] < -76.432312)
 
+#delete points
 coordinates.drop(coordinates[latitudeThresh & longitudeThresh | ~latitudeBoundry | ~longitudeBoundry].index, inplace = True)
 
-#latitudeThreshDupes = (abs(coordinates['Latitude'] - coordinates['Latitude'].shift(2)) < 0.0025) & sameDay
-#longitudeThreshDupes = (abs(coordinates['Longitude'] - coordinates['Longitude'].shift(2)) < 0.0025) & sameDay
-
-#coordinates.drop(coordinates[latitudeThreshDupes | latitudeThreshDupes].index, inplace = True)
-#coordinates.drop(coordinates[latitudeThresh | longitudeThresh].index, inplace = True)
-
+#delete any points where there is only one for a particular day (need atleast 2 to form a route)
 dupes = coordinates[coordinates.duplicated('Date', keep = False)]
 
-dupes.to_csv (r'export_dataframe_250m_Dupes.csv', index = False, header = True)
+#export to csv
+dupes.to_csv (r'export_dataframe_250m.csv', index = False, header = True)
